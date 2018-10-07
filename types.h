14b0bd8e59ce53d7648fc71683d2ab439f5d89fb
Jonathan Nilsson <jonathan@voysys.se>
2018-10-07 13:15:01 +0200

    With types

---
+++ types.h
@@ -0,0 +1,31 @@
+#pragma once
+
+typedef signed char i8;
+typedef unsigned char u8;
+
+typedef short i16;
+typedef unsigned short u16;
+
+typedef int i32;
+typedef unsigned int u32;
+
+typedef long long i64;
+typedef unsigned long long u64;
+
+typedef float r32;
+
+_Static_assert(sizeof(void *) == 8, "Error");
+
+_Static_assert(sizeof(i8) == 1, "Error");
+_Static_assert(sizeof(u8) == 1, "Error");
+
+_Static_assert(sizeof(i16) == 2, "Error");
+_Static_assert(sizeof(u16) == 2, "Error");
+
+_Static_assert(sizeof(i32) == 4, "Error");
+_Static_assert(sizeof(u32) == 4, "Error");
+
+_Static_assert(sizeof(i64) == 8, "Error");
+_Static_assert(sizeof(u64) == 8, "Error");
+
+_Static_assert(sizeof(r32) == 4, "Error");
